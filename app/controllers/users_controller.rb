c0a6151ca6de990517e19be678a9737a63271025
michi1985 <acoustic.revolution0322@gmail.com>
2018-04-24 23:58:04 +0900

    completed 7.1.2 #6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    p "==================="
+    p @user
+    p "==================="
+  end
+
+  def edit
+  end
+
 end
