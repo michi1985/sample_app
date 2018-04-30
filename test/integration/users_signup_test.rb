b26a98d98dc306660889bf14ff41ad809e428913
michi1985 <acoustic.revolution0322@gmail.com>
2018-04-30 14:18:43 +0900

    modify test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,11 +5,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
-      post signup_path, params: { user: {name: "", email: "user@invalid", password: "foobar", password_confirmation: "bar" } }
-    end
-    assert_template "users/new"
-    assert_select "div.alert", "The form contains 3 errors."
-    assert_select 'form[action="/signup"]'
+      post signup_path, params: { user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+  end
+
+  assert_template "users/new"
+  assert_select "div.alert", "The form contains 4 errors."
+  assert_select 'form[action="/signup"]'
+  assert_template 'users/new'
+  assert_select 'div#error_explanation'
+  assert_select 'div.field_with_errors'
+
+  assert_select 'ul' do
+    assert_select 'li', 'Name can\'t be blank'
+    assert_select 'li', 'Email is invalid'
+    assert_select 'li', 'Password confirmation doesn\'t match Password'
+    assert_select 'li', 'Password is too short (minimum is 6 characters)'
+  end
+
   end
 
   test "valid signup information" do
