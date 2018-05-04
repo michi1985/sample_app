27d3f0569760aa4213870507068b1c55afc988e0
michi1985 <acoustic.revolution0322@gmail.com>
2018-05-04 23:02:23 +0900

    completed basic-login #8

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,18 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
+  # 現在ログイン中のユーザを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  # 現在のユーザをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
