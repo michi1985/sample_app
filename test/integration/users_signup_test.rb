d0e15b65411c71003fb62e16160e3828e09fca17
michi1985 <acoustic.revolution0322@gmail.com>
2018-05-17 00:58:43 +0900

    completed 10.1.3 #11

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,21 +6,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference "User.count" do
       post signup_path, params: { user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
-  end
+    end
 
-  assert_template "users/new"
-  assert_select "div.alert", "The form contains 4 errors."
-  assert_select 'form[action="/signup"]'
-  assert_template 'users/new'
-  assert_select 'div#error_explanation'
-  assert_select 'div.field_with_errors'
+    assert_template "users/new"
+    assert_select "div.alert", "The form contains 4 errors."
+    # assert_select 'form[action="/signup"]'
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
 
-  assert_select 'ul' do
-    assert_select 'li', 'Name can\'t be blank'
-    assert_select 'li', 'Email is invalid'
-    assert_select 'li', 'Password confirmation doesn\'t match Password'
-    assert_select 'li', 'Password is too short (minimum is 6 characters)'
-  end
+    assert_select 'ul' do
+      assert_select 'li', 'Name can\'t be blank'
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', 'Password confirmation doesn\'t match Password'
+      assert_select 'li', 'Password is too short (minimum is 6 characters)'
+    end
 
   end
 
