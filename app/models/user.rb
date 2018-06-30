b14759a5859b8b9a9ffdf7c01bd439febfb07a8f
michi1985 <acoustic.revolution0322@gmail.com>
2018-06-30 20:48:43 +0900

    completed 14.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,4 +59,15 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  # ユーザのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  # ユーザをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
 end
